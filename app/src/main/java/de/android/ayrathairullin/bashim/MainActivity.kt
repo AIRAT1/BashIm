ee691d297f5f23fdb687f5c54109e11d6b184d82
Ayrat <ayrat1@mail.ru>
2018-02-28 16:52:19 +0300

    complete 02.08.00

--- app/src/main/java/de/android/ayrathairullin/bashim/MainActivity.kt
+++ app/src/main/java/de/android/ayrathairullin/bashim/MainActivity.kt
@@ -2,6 +2,7 @@ package de.android.ayrathairullin.bashim
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import butterknife.BindView
@@ -17,15 +18,19 @@ const val tag: String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     @BindView(R.id.recyclerView)
-    lateinit var recyclerView: RecyclerView
-    val compositeDisposable: CompositeDisposable = CompositeDisposable()
-    val repository: SearchRepositoty = SearchRepositoryProvider.provideSearchRepository();
+    lateinit var recyclerView : RecyclerView
+    val compositeDisposable : CompositeDisposable = CompositeDisposable()
+    val repository : SearchRepositoty = SearchRepositoryProvider.provideSearchRepository()
+    lateinit var adapter : SourceOfQuotesAdapter
     private val list: MutableList<SourceOfQuotes> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ButterKnife.bind(this)
+        val llm = LinearLayoutManager(this)
+        llm.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = llm
         compositeDisposable.add(
                 repository.searchSourcesOfQuotes()
                         .observeOn(AndroidSchedulers.mainThread())
@@ -34,6 +39,7 @@ class MainActivity : AppCompatActivity() {
                             result.forEach {
                                 list.addAll(it)
                             }
+                            recyclerView.adapter = SourceOfQuotesAdapter(list)
                             Log.d(tag, list.toString())
                         })
         )
