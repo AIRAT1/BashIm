d0187294c7c890595ccceb3053624fef24b9dd29
Ayrat <ayrat1@mail.ru>
2018-02-28 11:30:54 +0300

    complete 01.42.24

--- app/src/main/java/de/android/ayrathairullin/bashim/MainActivity.kt
+++ app/src/main/java/de/android/ayrathairullin/bashim/MainActivity.kt
@@ -2,7 +2,10 @@ package de.android.ayrathairullin.bashim
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
 import android.util.Log
+import butterknife.BindView
+import butterknife.ButterKnife
 import de.android.ayrathairullin.bashim.data.SearchRepositoryProvider
 import de.android.ayrathairullin.bashim.data.SearchRepositoty
 import de.android.ayrathairullin.bashim.data.SourceOfQuotes
@@ -10,22 +13,24 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
-const val tag : String = "MainActivity"
+const val tag: String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    val compositeDisposable : CompositeDisposable = CompositeDisposable()
-    val repository : SearchRepositoty = SearchRepositoryProvider.provideSearchRepository();
-
+    @BindView(R.id.recyclerView)
+    lateinit var recyclerView: RecyclerView
+    val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    val repository: SearchRepositoty = SearchRepositoryProvider.provideSearchRepository();
     private val list: MutableList<SourceOfQuotes> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
         compositeDisposable.add(
                 repository.searchSourcesOfQuotes()
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribeOn(Schedulers.io())
-                        .subscribe({result ->
+                        .subscribe({ result ->
                             result.forEach {
                                 list.addAll(it)
                             }
---
+++ app/src/main/java/de/android/ayrathairullin/bashim/SourceOfQuotesAdapter.kt
@@ -0,0 +1,9 @@
+package de.android.ayrathairullin.bashim
+
+import android.support.v7.widget.RecyclerView
+import de.android.ayrathairullin.bashim.data.SourceOfQuotes
+
+
+class SourceOfQuotesAdapter(list: MutableList<SourceOfQuotes>) : RecyclerView.Adapter<SourceOfQuotesAdapter.ViewHolder>() {
+
+}
